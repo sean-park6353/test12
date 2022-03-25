ae6c3a1343f8563dee5c5069fe30ef2402370610
parkjiseong6352 <hyoshincopy@nolbal.com>
2022-03-25 22:15:19 +0900

    my third commit

--- hello.py
+++ hello.py
@@ -1,3 +1,20 @@
 print('hello worlddddddd')
 print('hi')
 print("bye")
+
+
+
+
+"""
+[local => origin]
+git add <스테이지에 올릴 파일> => stage에 올라감
+git commit -m "커밋 메시지" => origin repository에 올라감
+git push  => 실제 origin에 밀어넣음
+
+
+[origin => local]
+
+git pull
+
+
+"""
